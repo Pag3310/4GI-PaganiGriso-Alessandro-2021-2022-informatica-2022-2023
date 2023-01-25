aa1e31773c9ea01862206eddaa6b59bf6a4aebdc
4gi-Alessandro-PaganiGriso <120627094+Pag3310@users.noreply.github.com>
2023-01-25 09:11:30 +0100

    Calculator fatto in classe

---
+++ info_git/project/bin/v1/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    private Contatore c;
+    private static final double COSTO = 3,5;
+    public Calculator(){
+        c= new Contatore();
+    }
+    public void start(){
+        double ris;
+        descizione();
+        input();
+        ris= calcolabBolletta();
+        VisRis();
+    }
+    public void input(){
+        Scanner s = new Scanner (System.in)
+        System.out.println()
+        
+    }
+}
