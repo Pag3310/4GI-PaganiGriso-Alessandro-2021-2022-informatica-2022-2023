14834e9973ffcfa5d002df71a248947ae9633466
4gi-Alessandro-PaganiGriso <120627094+Pag3310@users.noreply.github.com>
2023-02-07 08:31:34 +0100

    Contatore non è stato caricato correttamente

---
+++ info_git/project/bin/v1/Contatore.java
@@ -0,0 +1,35 @@
+public class Contatore{
+    private double valoreAttuale;
+    private double valorePrecedente;
+
+    public Contatore(){
+      this.valoreAttuale=0;
+       this.valorePrecedente=0;
+    }
+    public Contatore(double a, double p){
+        this.valoreAttuale=0;
+        this.valorePrecedente=0;
+        setValoreAttuale(a);
+        setValorePrecedente(p);
+    }
+    public void setValoreAttuale(double a){
+        if(a>0){
+            this.valoreAttuale=a;
+        }else{
+            this.valoreAttuale=0;
+        }
+    }
+    public void setValorePrecedente(double p){
+        if(p>0){
+            this.valorePrecedente=p;
+        }else{
+            this.valorePrecedente=0;
+        }
+    }
+    public getValoreAttuale(){
+        return this.valoreAttuale;
+    }
+    public getValorePrecedente(){
+        return this.valorePrecedente;
+    }
+}
